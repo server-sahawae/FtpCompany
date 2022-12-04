aec730e347ee75a1d7e7747be125b9bc07676454
sahawaenya <saha.waenya@gmail.com>
2022-12-04 17:22:01 +0700

    Done v1

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const express = require("express");
-const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 const Controller = require("./controller");
